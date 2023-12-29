704cd891082a9362eab73fa2493af339e5c6868b
Hoony0321 <eogns0321@gmail.com>
2023-12-30 01:00:12 +0900

    feat : add recoil library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import "@/styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import GlobalStyles from "@/styles/globalStyle";
+import Layout from "./components/Layout";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Layout router={pageProps.router}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
   );
 }
